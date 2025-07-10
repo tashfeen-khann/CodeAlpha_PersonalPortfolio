a80f360538d0077d71d550dfb2bf4916d6e66a0c
Tashfeen-khann <tashfeenkhan590@gmail.com>
2025-07-10 16:44:08 +0500

    thirt commit

--- src/pages/algocraft.jsx
+++ src/pages/algocraft.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import '../style/algocraft.css'
-import event from '../assets/imgs/event.png';
-import ecommerce from '../assets/imgs/ecommerce.png';
-import portfolio from '../assets/imgs/portfolio.png';
-import bakery from '../assets/imgs/bakery.png';
+
 import hero from '../assets/imgs/hero.png';
 import features from '../assets/imgs/features.png';
 import steps from '../assets/imgs/steps.png';
@@ -15,7 +12,7 @@ import arrayvis from '../assets/imgs/arrayvis.png';
 import graph from '../assets/imgs/graph.png';
 import gpt from '../assets/imgs/gpt.png';
 const images = [
-  hero,features,steps,about,footer,dsa,array,arrayvis,graph,gpt];
+  hero, features, steps, about, footer, dsa, array, arrayvis, graph, gpt];
 
 const algocraft = () => {
   return (
@@ -27,15 +24,15 @@ const algocraft = () => {
           <div className="box yellow wide"><img src={images[0]} alt="" /></div>
           <div className="box"><img src={images[1]} alt="" /></div>
           {/* <div className="box yellow tall"><img src={images[2]} alt="" /></div> */}
-                  <div className="box yellow"><img src={images[2]} alt="" /></div>
+          <div className="box yellow"><img src={images[2]} alt="" /></div>
 
           <div className="box big"><img src={images[3]} alt="" /></div>
           <div className="box"><img src={images[5]} alt="" /></div>
-                    <div className="box big"><img src={images[4]} alt="" /></div>
+          <div className="box big"><img src={images[4]} alt="" /></div>
           <div className="box"><img src={images[6]} alt="" /></div>
-                    <div className=" box"><img src={images[7]} alt="" /></div>
-                    <div className=" box three"><img src={images[8]} alt="" /></div>
-                    <div className=" box gpt"><img src={images[9]} alt="" /></div>
+          <div className=" box"><img src={images[7]} alt="" /></div>
+          <div className=" box three"><img src={images[8]} alt="" /></div>
+          <div className=" box gpt"><img src={images[9]} alt="" /></div>
 
           {/* <div className="box"><img src={images[8]} alt="" /></div> */}
         </div>
